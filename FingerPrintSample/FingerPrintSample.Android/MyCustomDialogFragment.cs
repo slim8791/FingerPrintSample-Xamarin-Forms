236842f518dc0f0c09825530a149ff7a19662524
slim8791 <slim.hammami.atj@outlook.fr>
2018-03-13 11:13:02 +0100

    Add project files.

---
+++ FingerPrintSample/FingerPrintSample.Android/MyCustomDialogFragment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Plugin.Fingerprint.Dialog;
+
+namespace FingerPrintSample.Droid
+{
+    public class MyCustomDialogFragment : FingerprintDialogFragment
+    {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
+            view.Background = new ColorDrawable(Color.Magenta);
+            return view;
+        }
+    }
+}
---
+++ FingerPrintSample/FingerPrintSample/MainPage.xaml.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Plugin.Fingerprint.Abstractions;
+using Xamarin.Forms;
+
+namespace FingerPrintSample
+{
+    public partial class MainPage : ContentPage
+    {
+        private CancellationTokenSource _cancel;
+        private bool _initialized;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private async void OnAuthenticate(object sender, EventArgs e)
+        {
+            await AuthenticationAsync("Put your finger!");
+
+        }
+        private async Task AuthenticationAsync(string reason, string cancel = null, string fallback = null, string tooFast = null)    
+        {
+            _cancel = swAutoCancel.IsToggled ? new CancellationTokenSource(TimeSpan.FromSeconds(10)) : new CancellationTokenSource();
+            lblStatus.Text = "";
+
+            var dialogConfig = new AuthenticationRequestConfiguration(reason)
+            { // all optional
+                CancelTitle = cancel,
+                FallbackTitle = fallback,
+                AllowAlternativeAuthentication = swAllowAlternative.IsToggled
+            };
+
+            // optional
+
+            var result = await Plugin.Fingerprint.CrossFingerprint.Current.AuthenticateAsync(dialogConfig, _cancel.Token);
+
+            await SetResultAsync(result);
+        }
+
+        private async Task SetResultAsync(FingerprintAuthenticationResult result)
+        {
+            if (result.Authenticated)
+            {
+                await DisplayAlert("FingerPrint Sample", "Success", "Ok");
+            }
+            else
+            {
+                await DisplayAlert("FingerPrint Sample", result.ErrorMessage, "Ok");
+            }
+        }
+    }
+}
